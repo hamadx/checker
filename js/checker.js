ea82c238355e0fd1249736aee0c9738f72754f5f
Sinderella <rawsec@gmail.com>
2015-11-21 18:19:00 +0000

    Initial commit

---
+++ js/checker.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#statement").keyup(function () {
+        var step2 = $("#step2");
+        var step3 = $("#step3");
+        var statement = $(this).val().split(' ');
+        var statement2 = [];
+        var password = "";
+        statement.forEach(function (entry) {
+            if (entry == "")
+                return;
+            statement2.push("<b>" + entry[0] + "</b>" + entry.substr(1));
+            password += entry[0];
+        });
+        statement2 = statement2.join(' ');
+        step2.html(statement2);
+        step3.text(password);
+    });
+});
