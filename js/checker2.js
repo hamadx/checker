c1738e7cd41d42fb19c2ed3806b5d2f774515199
hamadx <lparabia@live.com>
2015-12-03 08:59:34 +0000

    New interface and reorganised metrics for the passwords. Length, complexity, and unpredictability.
    Length: Between 1-14 where 14 is strong and 1 is very weak
    Complexity: alphabet, numbers, symbols, the more the better.
    Unpredictability: if a word, or many words separated by non-alphabets were found in a dictionary then it's considered weak.

--- js/checker2.js
+++ js/checker2.js
@@ -1,39 +1,75 @@
 $(document).ready(function () {
+
+    var termsCount=0;
+    var dictTermsCount=0;
+    var opts = {
+        lines: 10,
+        angle: 0.15,
+        lineWidth: 0.34,
+        pointer: {
+            length: 0.4,
+            strokeWidth: 0.055,
+            color: '#000000'
+        },
+        limitMax: 'false',
+        percentColors: [[0.0, "#ff0000" ], [0.50, "#ff8000"], [1.0, "#00ff00"]], // !!!!
+        strokeColor: '#ff0000',
+        generateGradient: true
+    };
+
+    var target1 = document.getElementById('complexityGauge');
+    var gauge1 = new Gauge(target1).setOptions(opts);
+    gauge1.maxValue = 2;
+    gauge1.animationSpeed = 32;
+
+    var target2 = document.getElementById('lengthGauge');
+    var gauge2 = new Gauge(target2).setOptions(opts);
+    gauge2.maxValue = 14;
+    gauge2.animationSpeed = 32;
+
+    var target3 = document.getElementById('unpredictabilityGauge');
+    var gauge3 = new Gauge(target3).setOptions(opts);
+
+
     function stripPassword(pwd) {
         return (pwd.replace(/[^a-zA-Z ]/g, ""));
     }
 
+    function splitToWords(pwd){
+        return pwd.split(/[^a-zA-Z]+/g); // Any non-alphabet char is considered a separator
+    }
+
     function getPasswordStrength(pwd) {
-        var passwordStrength = "";
+        var passwordStrength = -1;
 
         var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
         var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
 
         if (strongRegex.test(pwd)) {
-            passwordStrength += "Strong";
+            passwordStrength = 2; //strong
         } else if (mediumRegex.test(pwd)) {
-            passwordStrength += "Medium";
+            passwordStrength = 1; //medium
         } else {
-            passwordStrength += "Weak";
+            passwordStrength = 0; //weak
         }
 
         return passwordStrength;
     }
 
-    function lookupDict(word) {
+    function lookupDict(word,callback) {
         var dictAPI = "http://api.wordnik.com:80/v4/words.json/search/" + word + "?caseSensitive=false&minCorpusCount=5&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=1&maxLength=-1&skip=0&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
         $.getJSON(dictAPI, function (data) {
                 console.log("success");
                 var inDict = (data.totalResults > 0); // if word is found in dictionary
                 //console.log(data.totalResults,inDict);
-                updateDict(inDict);
+                callback(inDict);
             })
             .done(function () {
                 console.log("second success");
             })
             .fail(function () {
                 console.log("error");
-                updateDict(error);
+                callback(error);
             })
             .always(function () {
                 console.log("complete");
@@ -41,15 +77,27 @@ $(document).ready(function () {
     }
 
     $("#pwdInput").keyup(function () {
-        // window.alert("sometext");
-        var m = $('#label1');
+        //alert("455");
+       // var m = $('#label1');
         var i = $('#pwdInput');
-        //alert(getPasswordStrength(i.val()));
-        m.text(getPasswordStrength(i.val()));
+        var s=getPasswordStrength(i.val());
+        /**switch(s){
+            case 0: m.text('Weak'); break;
+            case 1: m.text('Medium'); break;
+            case 2: m.text('Strong'); break;
+            default: m.text('Error');
+        }*/
+
+        gauge1.set(s);
+
+        var l= i.val().length;
+        if (l>14) l=14;
+
+        gauge2.set(l);
+
     });
 
     $("#btn1").click(function () {
-        // alert(stripPassword("abcdedfg"));
         var m = $('#label2');
         var i = $('#pwdInput');
 
@@ -60,19 +108,42 @@ $(document).ready(function () {
         var m = $('#label2');
         switch (ret) {
             case true:
-                m.text("You chose a password that is easily found in English dictionary, bad idea!");
+                m.append("You chose a password that is easily found in English dictionary, bad idea!");
+
                 break;
             case false:
-                m.text("Password is not found in a dictionary, good thing!");
+                m.append("Password is not found in a dictionary, good thing!");
+                --dictTermsCount;
+                console.log("dictTermCount: " + dictTermsCount);
                 break;
             default:
-                m.text("Error with Dictionary API!");
+                m.append("Error with Dictionary API!");
         }
+
+        var target3 = document.getElementById('unpredictabilityGauge');
+        var gauge3 = new Gauge(target3).setOptions(opts);
+
+        gauge3.animationSpeed = 32;
+        gauge3.maxValue = termsCount;
+        gauge3.set(termsCount-dictTermsCount);
     }
 
-    $("#btn2").click(function () {
+    $("#checkBtn").click(function () {
+
         var i = $('#pwdInput');
-        lookupDict(i.val());
+        var terms = splitToWords(i.val());
+        var removeItem = "";
+
+        terms = jQuery.grep(terms, function(value) {
+            return value != removeItem;
+        });
+        termsCount=terms.length;
+        dictTermsCount=termsCount;
+        console.log(terms);
+        $.each(terms, function( index, value ) {
+            lookupDict(value,updateDict);
+        });
+
     });
 
     $("#btn3").click(function () {
