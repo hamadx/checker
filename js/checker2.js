1cbfd5f013df190aa25ac61f7b7147ae8dc13135
hamadx <lparabia@live.com>
2015-12-04 11:12:05 +0000

    changes

--- js/checker2.js
+++ js/checker2.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+    var pwdFlags= {
+        length:0,
+        complexity:0,
+        unpredictability: false
+    };
+
+    var dictionary;
+    var terms;
+    $("#dictionary").load("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_1000000.txt", function(){
+        var text = $("#dictionary").text();
+        var words = text.split("\n");
+        dictionary = new Array();
+        for(var i=0; i < words.length; i++) {
+            dictionary[i] = words[i];
+        }
+        $("#loader").fadeOut();
+        $("#pwdInput").fadeIn();
+        var ns= document.getElementById('pwdInput').offsetHeight + "px";
+        $("#inputBox").animate({height: ns },1000)
+    });
+
+
+    function dic(displayComment) {
+        var word = document.getElementById("pwdInput").value;
+        // var dictionary = new Array("Java","Python","Swift","HTML","PHP");
+        // This line is unnecessary; use the dictionary var already created.
+        var flag = 0;
+        for(var i = 0; i < dictionary.length; i++) {
+            if(dictionary[i] == word) {
+                if (displayComment)
+                    $("#outputMessage").append("&#149; Woah, your password is such a cliche. Just try googling it and you will find many people use it.<br>");
+                flag = 1;
+                break;
+            }
+/*            if(i == dictionary.length - 1) {
+                if(displayComment)
+                $("#outputMessage").append("Ha! Original password! Good one <br>");
+            }*/
+        }
+    }
 
     var termsCount=0;
     var dictTermsCount=0;
@@ -17,14 +57,14 @@ $(document).ready(function () {
         generateGradient: true
     };
 
-    var target1 = document.getElementById('complexityGauge');
+    var target1 = document.getElementById('lengthGauge');
     var gauge1 = new Gauge(target1).setOptions(opts);
-    gauge1.maxValue = 2;
+    gauge1.maxValue = 14;
     gauge1.animationSpeed = 32;
 
-    var target2 = document.getElementById('lengthGauge');
+    var target2 = document.getElementById('complexityGauge');
     var gauge2 = new Gauge(target2).setOptions(opts);
-    gauge2.maxValue = 14;
+    gauge2.maxValue = 2;
     gauge2.animationSpeed = 32;
 
     var target3 = document.getElementById('unpredictabilityGauge');
@@ -58,60 +98,124 @@ $(document).ready(function () {
         return passwordStrength;
     }
 
-    function lookupDict(word,callback) {
+    function lookupDict(word,index,callback,displayComment) {
         var dictAPI = "http://api.wordnik.com:80/v4/words.json/search/" + word + "?caseSensitive=false&minCorpusCount=5&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=1&maxLength=-1&skip=0&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
         $.getJSON(dictAPI, function (data) {
                 console.log("success");
                 var inDict = (data.totalResults > 0); // if word is found in dictionary
-                //console.log(data.totalResults,inDict);
-                callback(inDict);
+                callback(inDict,index,displayComment);
             })
             .done(function () {
                 console.log("second success");
             })
             .fail(function () {
-                console.log("error");
-                callback(error);
+                console.log("Error with API Call");
+                callback("Error with API Call");
             })
             .always(function () {
                 console.log("complete");
             });
     }
-    function unpTest() {
+    function unpTest(displayComment) {
 
         var i = $('#pwdInput');
-        var terms = splitToWords(i.val());
-        var removeItem = "";
+        terms = splitToWords(i.val());
+       // var removeItem = "";
 
         terms = jQuery.grep(terms, function(value) {
-            return value != removeItem;
+            return value.length > 2;// remove empty and 2 letter items
         });
+        console.log(terms);
         termsCount=terms.length;
         dictTermsCount=termsCount;
-        console.log(terms);
+
+        if(terms.length>0){
         $.each(terms, function( index, value ) {
-            lookupDict(value,updateDict);
+            lookupDict(value,index,updateDict,displayComment);
         });
+        }
+        else
+        {
+            var gauge3 = new Gauge(target3).setOptions(opts);
+            gauge3.animationSpeed = 32;
+            gauge3.set(0);
+        }
+       dic(displayComment);
 
     }
     var pwdInput = $('#pwdInput');
+
+    function getWiseCatMessage(){
+        switch (true)
+        {
+            case(pwdFlags.length<=3): // Length 1-3
+                if (pwdFlags.complexity==0)  // Low Complexity
+                    return outMessages[0];
+                else                        // Medium and High Complexity
+                    return (pwdFlags.unpredictability?outMessages[1]:outMessages[2]);
+            case (pwdFlags.length>=4 && pwdFlags.length<=7): // length 4-7
+                switch(pwdFlags.complexity){
+                    case 0: /// Low Complexity
+                        return (pwdFlags.unpredictability?outMessages[3]:outMessages[4]);
+                    case 1: // Medium Complexity
+                        return (pwdFlags.unpredictability?outMessages[5]:outMessages[6]);
+                    case 2: // High Complexity
+                        return (pwdFlags.unpredictability?outMessages[7]:outMessages[8]);
+                }
+            case (pwdFlags.length>=8 && pwdFlags.length<=10): // Length 8-10
+                switch(pwdFlags.complexity){
+                    case 0: /// Low Complexity
+                        return (pwdFlags.unpredictability?outMessages[9]:outMessages[10]);
+                    case 1: // Medium Complexity
+                        return (pwdFlags.unpredictability?outMessages[11]:outMessages[12]);
+                    case 2: // High Complexity
+                        return (pwdFlags.unpredictability?outMessages[13]:outMessages[14]);
+                }
+            case (pwdFlags.length>=8 && pwdFlags.length<=10): // Length 11-12
+                switch(pwdFlags.complexity){
+                    case 0: /// Low Complexity
+                        return (pwdFlags.unpredictability?outMessages[15]:outMessages[16]);
+                    case 1: // Medium Complexity
+                        return (pwdFlags.unpredictability?outMessages[17]:outMessages[18]);
+                    case 2: // High Complexity
+                        return (pwdFlags.unpredictability?outMessages[19]:outMessages[20]);
+                }
+            case (pwdFlags.length>=13 ): // Length >= 13
+                switch(pwdFlags.complexity){
+                    case 0: /// Low Complexity
+                        return (pwdFlags.unpredictability?outMessages[21]:outMessages[22]);
+                    case 1: // Medium Complexity
+                    case 2: // High Complexity
+                        return (pwdFlags.unpredictability?outMessages[23]:outMessages[24]);
+                }
+        }
+    }
+
     pwdInput.lastValue = "";
     var timeout;
-    pwdInput.keyup(function () {
-       // var m = $('#label1');
+    pwdInput.keyup(function (event) {
+        if(event.keyCode == 13){
+            //alert(getWiseCatMessage());
+            $("#outputMessage").text("");
+            console.log(pwdFlags);
+            $("#outputMessage").append(getWiseCatMessage()).append("<hr>");
+            unpTest(true);
+        }
         var i = pwdInput.val();
-        if(i !=  pwdInput.lastValue) {
+       //if(i !=  pwdInput.lastValue) {
             // Save the "last" value
             $('#pwdInput').lastValue = i;
             // Delay before search in the case of typing
             if(timeout) { clearTimeout(timeout); }
             // Start new time out
             timeout = setTimeout(function() {
-                // Do the search!
-                console.warn("Doing search for " + i + ", time waited");
-                unpTest();
+                unpTest(false);
             },1000);
-        }
+        //}
+/*        if(pwdInput.val().length==0) {
+            termsCount=dictTermsCount=0;
+            gauge3.set(0);
+        }*/
         var s=getPasswordStrength(i);
         /**switch(s){
             case 0: m.text('Weak'); break;
@@ -119,34 +223,34 @@ $(document).ready(function () {
             case 2: m.text('Strong'); break;
             default: m.text('Error');
         }*/
-
-        gauge1.set(s);
-
         var l= i.length;
         if (l>14) l=14;
-
-        gauge2.set(l);
-
+       // console.log("Length="+l, "Complexity"+s)
+        gauge1.set(l);
+        gauge2.set(s);
+        pwdFlags.length=l;
+        pwdFlags.complexity=s;
     });
 
+
     $("#btn1").click(function () {
-        var m = $('#label2');
+        var m = $('#outputMessage');
         var i = $('#pwdInput');
 
         m.text(stripPassword(i.val()));
     });
 
-    function updateDict(ret) {
-        var m = $('#label2');
+    function updateDict(ret,index,displayComment){
+    //alert(ret);
+        var m = $('#outputMessage');
         switch (ret) {
             case true:
-                m.append("You chose a password that is easily found in English dictionary, bad idea!");
-
+                if (displayComment)
+                m.append("&#149; I found the word <em><b>" + terms[index] + "</b></em> in your password that is easily found in English dictionary, bad idea!<br>");
                 break;
             case false:
-                m.append("Password is not found in a dictionary, good thing!");
+               // m.append("Password is not found in a dictionary, good thing!<br>");
                 --dictTermsCount;
-                console.log("dictTermCount: " + dictTermsCount);
                 break;
             default:
                 m.append("Error with Dictionary API!");
@@ -156,10 +260,13 @@ $(document).ready(function () {
         var gauge3 = new Gauge(target3).setOptions(opts);
 
         gauge3.animationSpeed = 32;
+        gauge3.render();
         gauge3.maxValue = termsCount;
-        gauge3.set(termsCount-dictTermsCount);
-    }
+        gauge3.set(termsCount-dictTermsCount)
+       // console.log("Text: " + $("#pwdInput").val());
 
+        pwdFlags.unpredictability=((termsCount-dictTermsCount)>=gauge3.maxValue);
+    }
 
 
     $("#btn3").click(function () {
